1ecd5a2e102ccda525b16dce7e7421dc9d5344bf
unknown <farhanbsyr@gmail.com>
2024-07-05 06:01:03 +0700

    perbaiki hero

--- src/component/hero/HeroText.jsx
+++ src/component/hero/HeroText.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function HeroText() {
   return (
-    <div className="h-description ">
-      <h4 className="text-left text-2xl">
-        Hello, I'm <span className="font-semibold text-3xl"> Farhan,</span>{" "}
+    <div className="h-description mr-10">
+      <h4 className="text-left text-xl">
+        Hello, I'm <span className="font-semibold text-2xl"> Farhan,</span>{" "}
         <br />
         I'm a fresh graduate who is studying the field of website development,
         especially the{" "}
-        <span className="font-semibold text-3xl">Frontend Developer</span>{" "}
+        <span className="font-semibold text-2xl">Frontend Developer</span>{" "}
       </h4>
     </div>
   );
