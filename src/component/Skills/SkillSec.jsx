b4bcf4131127193816a0abeea58511b296f3c1a6
unknown <farhanalktr91@gmail.com>
2024-07-29 15:05:39 +0700

    menambahkan icon pada skill section

--- src/component/Skills/SkillSec.jsx
+++ src/component/Skills/SkillSec.jsx
@@ -5,7 +5,7 @@ import Ireact from "../../assets/physics.png";
 import html from "../../assets/html-icon.svg";
 import tailwincss from "../../assets/tailwind-css-icon.svg";
 import bootsrap from "../../assets/bootstrap-5-logo-icon.svg";
-
+import vue from "../../assets/vue-dot-js.svg";
 function SkillSec() {
   return (
     <div className="border-2 rounded-md shadow-lg p-6 border-gray-200 w-[30%]">
@@ -15,6 +15,7 @@ function SkillSec() {
         <SkillIcon nameIcon={"tailwindcss"} icon={tailwincss} widSkill={60} />
         <SkillIcon nameIcon={"bootsrap"} icon={bootsrap} widSkill={60} />
         <SkillIcon nameIcon={"html"} icon={html} widSkill={60} />
+        <SkillIcon nameIcon={"vue js"} icon={vue} widSkill={60} />
       </div>
     </div>
   );
