292ef6e0fb2141315e057c28405b4c5a5d5b18b3
unknown <farhanalktr91@gmail.com>
2024-07-29 15:18:34 +0700

    menambahkan icon di section skills

--- src/component/Skills/SkillSec.jsx
+++ src/component/Skills/SkillSec.jsx
@@ -6,16 +6,20 @@ import html from "../../assets/html-icon.svg";
 import tailwincss from "../../assets/tailwind-css-icon.svg";
 import bootsrap from "../../assets/bootstrap-5-logo-icon.svg";
 import vue from "../../assets/vue-dot-js.svg";
+import css from "../../assets/css-icon.svg";
 function SkillSec() {
   return (
-    <div className="border-2 rounded-md shadow-lg p-6 border-gray-200 w-[30%]">
-      <div className="flex flex-wrap gap-6 items-center h-full justify-center">
-        <SkillIcon nameIcon={"javascript"} icon={javascript} widSkill={60} />
-        <SkillIcon nameIcon={"react"} icon={Ireact} widSkill={60} />
-        <SkillIcon nameIcon={"tailwindcss"} icon={tailwincss} widSkill={60} />
-        <SkillIcon nameIcon={"bootsrap"} icon={bootsrap} widSkill={60} />
-        <SkillIcon nameIcon={"html"} icon={html} widSkill={60} />
-        <SkillIcon nameIcon={"vue js"} icon={vue} widSkill={60} />
+    <div className="border-2 rounded-md shadow-lg p-6 border-gray-200 w-[30%] ">
+      <div className=" my-auto flex items-center h-full">
+        <div className="flex flex-wrap gap-6 items-center  justify-center">
+          <SkillIcon nameIcon={"javascript"} icon={javascript} widSkill={55} />
+          <SkillIcon nameIcon={"react"} icon={Ireact} widSkill={55} />
+          <SkillIcon nameIcon={"vue js"} icon={vue} widSkill={55} />
+          <SkillIcon nameIcon={"tailwindcss"} icon={tailwincss} widSkill={55} />
+          <SkillIcon nameIcon={"bootsrap"} icon={bootsrap} widSkill={55} />
+          <SkillIcon nameIcon={"html semantice"} icon={html} widSkill={55} />
+          <SkillIcon nameIcon={"css"} icon={css} widSkill={55} />
+        </div>
       </div>
     </div>
   );
